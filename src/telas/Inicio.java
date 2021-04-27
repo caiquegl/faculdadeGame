9f5704a9d24da4901a144f84d17d005b57237952
Nienoque <nicolasenoque33@gmail.com>
2021-04-27 19:14:32 -0300

    botao

--- src/telas/Inicio.java
+++ src/telas/Inicio.java
@@ -37,7 +37,7 @@ public class Inicio extends javax.swing.JFrame {
         setResizable(false);
         getContentPane().setLayout(null);
 
-        jButton1.setText("jButton1");
+        jButton1.setText("inicio");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
